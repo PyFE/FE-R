0cb765693d0668ea33849cb61b0eac2efba20aef
jaehyukchoi <jaehyukchoi@users.noreply.github.com>
2021-02-16 18:46:53 +0800

    Reference for the test values added

--- pkg/tests/testthat/test-SABR.R
+++ pkg/tests/testthat/test-SABR.R
@@ -1,9 +1,11 @@
 library(FER)
 
+# Table 1 in Antonov & Spector (2012). https://ssrn.com/abstract=2026350
 test_that("SabrHagan2002 BS Volatility Table 1", {
   value <- round( FER::SabrHagan2002(
     strike=seq(0.1, 2, 0.1), spot=1, texp=10, sigma=0.25,
-    vov=0.3, rho=-0.8, beta=0.3), digits = 4 )
+    vov=0.3, rho=-0.8, beta=0.3),
+    digits = 4 )
   value2 <- c(
     0.7176, 0.5725, 0.4886, 0.4293, 0.3835,
     0.3462, 0.3148, 0.2876, 0.2638, 0.2427,
@@ -12,10 +14,12 @@ test_that("SabrHagan2002 BS Volatility Table 1", {
   expect_equal(value, value2, tolerance = 1e-5)
 })
 
+# Table 13 in Antonov & Spector (2012). https://ssrn.com/abstract=2026350
 test_that("SabrHagan2002 BS Volatility Table 13", {
   value <- round( FER::SabrHagan2002(
     strike=seq(0.1, 2, 0.1), spot=1, texp=20, sigma=0.25,
-    vov=0.3, rho=-0.5, beta=0.3), digits=4 )
+    vov=0.3, rho=-0.5, beta=0.3),
+    digits=4 )
   value2 <- c(
     0.7603, 0.5973, 0.5078, 0.4463, 0.3997,
     0.3626, 0.332, 0.3063, 0.2844, 0.2658,
